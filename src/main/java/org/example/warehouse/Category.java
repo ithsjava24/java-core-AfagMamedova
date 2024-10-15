b072e00d071e6775330a072140530c937b5b8f6d
AfagMamedova-JU23GBG <afag.mamedova@iths.se>
2024-10-15 21:25:51 +0200

    Laboration 2

--- src/main/java/org/example/warehouse/Category.java
+++ src/main/java/org/example/warehouse/Category.java
@@ -9,18 +9,17 @@ public class Category {
     private final String name;
 
     private Category(String name) {
-        if (name == null || name.trim().isEmpty()) {
-            throw new IllegalArgumentException("Category name can't be null or empty.");
+        if (name == null) {
+            throw new IllegalArgumentException("Category name can't be null");
         }
-        this.name = name;
-    }
-
-    public static Category getInstance(String name){
-        return instances.computeIfAbsent(name, Category::new);
+        this.name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
     }
 
     public static Category of(String name) {
-        return new Category(name);
+        if (name == null) {
+            throw new IllegalArgumentException("Category name can't be null");
+        }
+        return instances.computeIfAbsent(name, Category::new);
     }
 
     public String getName() {
@@ -32,7 +31,7 @@ public class Category {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Category category = (Category) o;
-        return name.equals(category.name);
+        return Objects.equals(name,category.name);
     }
 
     @Override

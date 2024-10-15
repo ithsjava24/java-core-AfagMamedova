8bb4e85c22dcb72563a0c4c80f443ff6d7ecae04
AfagMamedova-JU23GBG <afag.mamedova@iths.se>
2024-10-15 11:13:56 +0200

    Laboration 2

---
+++ src/main/java/org/example/warehouse/Category.java
@@ -0,0 +1,38 @@
+package org.example.warehouse;
+
+import java.util.Objects;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Category {
+    private static final Map<String, Category> categories = new HashMap<>();
+    private final String name;
+
+    private Category(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Category name can't be null or empty.");
+        }
+        this.name = name;
+    }
+
+    public static Category of(String name) {
+        return new Category(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return name.equals(category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
